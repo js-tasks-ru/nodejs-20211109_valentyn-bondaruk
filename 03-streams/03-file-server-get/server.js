4f2ff5aaa1ee114236e42876d0b46895f3b93296
BondVal <b.r.v.12.06@gmail.com>
2021-11-29 22:59:53 +0200

    added solution for 3-stream 3-file-server-get

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,8 +11,32 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const readFileStream = fs.createReadStream(filepath);
+
   switch (req.method) {
     case 'GET':
+      readFileStream
+          .on('error', (err) => {
+            if (err) {
+              if (pathname.includes('/')) {
+                res.statusCode = 400;
+                res.end('400');
+                return;
+              } else {
+                res.statusCode = 404;
+                res.end('404');
+                return;
+              }
+            }
+
+            res.statusCode = 200;
+            res.end(data);
+          })
+          .pipe(res);
+
+      req.on('aborted', () => {
+        readFileStream.destroy();
+      });
 
       break;
 
