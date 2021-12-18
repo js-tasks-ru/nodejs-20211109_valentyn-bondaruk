a773e6818467baa1acfca8ce08d76ae4ed86f810
valentynbondaruk <b.r.v.12.06@gmail.com>
2021-12-18 18:34:08 +0200

    Added solution 08-email 01-registration

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -1,11 +1,31 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const verificationToken = uuid();
+  const {email, displayName, password} = ctx.request.body;
+  const createdUser = await User.create({email, displayName, verificationToken});
+  await createdUser.setPassword(password);
 
+  await sendMail({to: email, subject: 'confirmation', template: 'confirmation', verificationToken});
+
+  ctx.user = createdUser;
+  ctx.status = 200;
+  ctx.body = {};
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const {verificationToken} = ctx.request.body;
+  const currentUser = await User.findOne({verificationToken});
 
+  currentUser.verificationToken = undefined;
+  await currentUser.save();
+
+  ctx.body = {token: verificationToken};
+
+  if (!currentUser) {
+    ctx.status = 400;
+    ctx.body = {error: 'Ссылка подтверждения недействительна или устарела'};
+  }
 };
